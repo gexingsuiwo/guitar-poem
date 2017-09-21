24e726054401bd8c8673ad1e2af940301bb19a1a
王刘洋 <447998931@qq.com>
2017-09-21 18:00:59 +0800

    吉他曲谱下载

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # guitar
+
+可以按照以下步骤来部署和运行程序:
+1.请确保机器有python3坏境
+2.确保有BeautifulSoup urllib模块，可用pip工具安装,很方便
+3.执行 python spider.py
+4.去吉他曲谱目录看看吧
+
+没了，就是这么简单!
--- spider.py
+++ spider.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-from http import cookiejar
 from urllib import parse
 from urllib import request
 from bs4 import BeautifulSoup
-import re
-#导入re模块
-import re
 import os
 import sys
 
@@ -18,8 +14,6 @@ class Spider:
         # Headers信息
         self.head = {'User-Agent': user_agent, 'Connection': 'keep-alive'}
 
-        pass
-    #
     def mkdir(self, path):
         isExist = os.path.exists(path)
         if not isExist:
@@ -44,7 +38,6 @@ class Spider:
 
         soup = BeautifulSoup(data, "html.parser")
         e = soup.select('.device img')
-        print(e)
         if len(e) > 0:
             #创建目录
             path = sys.path[0] + '/吉他曲谱/' + self.keyword + '/' + title
@@ -59,26 +52,28 @@ class Spider:
                 imgurl = 'http://www.ccguitar.cn/' + item.get('src')
                 alt = item.get('alt')
 
-                print(imgurl)
-
                 req1 = request.Request(imgurl, headers=self.head)
                 reponse = request.urlopen(req1)
                 data = reponse.read()
 
                 filename = "{}/{}".format(path, os.path.basename(imgurl))
+                print('+'+os.path.basename(imgurl))
 
                 with open(filename, 'wb') as f:
                     f.write(data)
 
+            print('下载完成')
+            print('')
         else:
             print("很抱歉，没有曲谱哦~")
+
 
     def getPage(self):
         self.keyword = input('请输入歌曲名：')
         keyword = parse.quote(self.keyword.encode('gbk'))
         url = "http://so.ccguitar.cn/tosearch.aspx?searchname=%s&pu_gs=3&seachtype=1" % keyword
 
-        #创建Request对象
+        #搜索
         req = request.Request(url=url, headers=self.head)
         response = request.urlopen(req)
 
@@ -95,8 +90,9 @@ class Spider:
             print("很抱歉，没有相关曲谱哦~")
             self.getPage()
 
-spider = Spider()
-spider.getPage()
+if __name__ == '__main__':
+    spider = Spider()
+    spider.getPage()
 
 
 
